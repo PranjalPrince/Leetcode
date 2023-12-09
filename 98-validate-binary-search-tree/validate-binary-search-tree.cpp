98d199198da4bc936e5b670aaa2e2e38ab7a98ea
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2023-12-09 21:22:42 +0530

    Time: 8 ms (68.56%) | Memory: 22.3 MB (15.18%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,22 +9,64 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         vector<int>v;
+//         helper(root,v);
+//         for(int i=0;i<v.size()-1;i++){
+//             if(v[i+1]<=v[i])
+//             return false;
+//         }
+//         return true;
+//     }
+//     void helper(TreeNode* root,vector<int>&v){
+//         if(root!=nullptr){
+//             helper(root->left,v);
+//             v.push_back(root->val);
+//             helper(root->right,v);
+//         }
+//     }
+// };
 class Solution {
-public:
-    bool isValidBST(TreeNode* root) {
-        vector<int>v;
-        helper(root,v);
-        for(int i=0;i<v.size()-1;i++){
-            if(v[i+1]<=v[i])
-            return false;
-        }
-        return true;
+    public:
+    long long ans=LONG_MIN;
+    void helper(TreeNode* root, bool& isvalid){
+        if(root==nullptr)
+        return;
+        helper(root->left,isvalid);
+        if(ans>=root->val)
+        isvalid=false;
+        ans=root->val;
+        helper(root->right,isvalid);
     }
-    void helper(TreeNode* root,vector<int>&v){
-        if(root!=nullptr){
-            helper(root->left,v);
-            v.push_back(root->val);
-            helper(root->right,v);
-        }
+    bool isValidBST(TreeNode* root){
+        bool isvalid=true;
+        helper(root,isvalid);
+        return isvalid;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
