9135b0e300fc98b19b5aca1c408f000887bd4171
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-01-10 13:49:18 +0530

    Time: 3 ms (66.03%) | Memory: 13.5 MB (19.37%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -10,25 +10,21 @@
  * };
  */
 class Solution {
+    void checkTree(TreeNode* node,vector<int>&vec){
+        if(!node)
+        return;
+        if(node->left==nullptr and node->right==nullptr){
+            vec.push_back(node->val);
+            return;
+        }
+        checkTree(node->left,vec);
+        checkTree(node->right,vec);
+    }
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> nodeList_root1, nodeList_root2;
-        findLeaf_dfs(root1, nodeList_root1);
-        findLeaf_dfs(root2, nodeList_root2);
-        return nodeList_root1 == nodeList_root2;
-    }
-
-    void findLeaf_dfs(TreeNode* nodeNow, std::vector<int>& nodeList) {
-        if (!nodeNow) {
-            return;
-        }
-
-        if (!nodeNow->left && !nodeNow->right) {
-            nodeList.push_back(nodeNow->val);
-            return;
-        }
-
-        findLeaf_dfs(nodeNow->left, nodeList);
-        findLeaf_dfs(nodeNow->right, nodeList);
+        vector<int>v1,v2;
+        checkTree(root1,v1);
+        checkTree(root2,v2);
+        return v1==v2;
     }
 };
