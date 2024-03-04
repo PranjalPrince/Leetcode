c90f72ebf1d442d93114525099f80787ba6c15be
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-03-04 21:04:54 +0530

    Time: 4 ms (74.87%) | Memory: 13 MB (55.08%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& v, int p) {
+        if(v.size()==1 and p>v[0])
+        return 1;
+        if(v.size()==0)
+        return 0;
+        bool b=true;
+        sort(begin(v),end(v));
+        int ans=0,i=0,j=v.size()-1;
+        while(true and i<=j){
+            if(v[i]>p and ans<=0)
+            break;
+            else if(v[i]<=p){
+                p-=v[i];
+                ans++;
+                b=true;
+                i++;
+            }
+            else{
+                p+=v[j];
+                ans--;
+                b=false;
+                j--;
+            }
+        }
+        if(b==false)
+        ans++;
+        return ans;
+    }
+};
