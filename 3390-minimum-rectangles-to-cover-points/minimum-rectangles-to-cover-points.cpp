bbe30deee31e417db3befc5988a684fc40377938
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-04-15 20:03:59 +0530

    Time: 253 ms (80.00%) | Memory: 131.4 MB (60.00%) - LeetSync

--- 3390-minimum-rectangles-to-cover-points/minimum-rectangles-to-cover-points.cpp
+++ 3390-minimum-rectangles-to-cover-points/minimum-rectangles-to-cover-points.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minRectanglesToCoverPoints(vector<vector<int>>& p, int w) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
+        cout.tie(NULL);
         sort(p.begin(),end(p));
         int ans=0,last=-1;;
         for(auto x:p){
