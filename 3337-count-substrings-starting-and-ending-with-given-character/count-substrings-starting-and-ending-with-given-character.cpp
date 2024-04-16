3abeca1babc8c4b68e499017d6e7fe79991fbf61
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-04-16 20:13:07 +0530

    Time: 5 ms (97.39%) | Memory: 11.5 MB (12.65%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,9 +1,15 @@
+auto speedrun=[](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
 class Solution {
 public:
     // using i64=int64_t;
     // constexpr i64 mod = 1000000007;
-    typedef long long int ll;
-    const ll mod=10000000;
+    // typedef long long int ll;
+    // const ll mod=10000000;
     long long countSubstrings(string s, char c) {
         long long res,ans=0,cnt=0;
         for(int i=0;i<s.size();i++){
