fd34036e411687f488b0fd366667e18bed3957c4
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-04-16 21:18:58 +0530

    Time: 83 ms (20.71%) | Memory: 18.6 MB (30.74%) - LeetSync

---
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        map<char,int>mp;
+        vector<int>v;
+        for(int i=0;i<word.size();i++){
+            mp[word[i]]++;
+        }
+        for(auto [key,val]:mp){
+            v.push_back(val);
+        }
+        // int n;
+        sort(begin(v),end(v));
+        if(v.size()==1)
+        return 0;
+        for(auto x: v) cout<<x<<' ';
+        int ans=INT_MAX;
+        for(int i=v[0];i<=v[v.size()-1];i++){
+            int cnt=0;
+            for(int j=0;j<v.size();j++){
+                if(v[j]<i){
+                    cnt+=v[j];
+                }
+                else if(v[j]>=i+k){
+                    cnt+=v[j]-(i+k);
+                }
+            }
+            // int n=word.size()-cnt;
+            ans=min(ans,cnt);
+        }
+        return ans;
+    }
+};
