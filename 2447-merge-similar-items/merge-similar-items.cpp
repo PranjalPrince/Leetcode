82b8167344b609a08064ed56dc0393b2d7f49259
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-04-17 12:26:47 +0530

    Time: 20 ms (69.21%) | Memory: 21.5 MB (21.66%) - LeetSync

--- 2447-merge-similar-items/merge-similar-items.cpp
+++ 2447-merge-similar-items/merge-similar-items.cpp
@@ -7,22 +7,31 @@ auto speed=[](){
 class Solution {
 public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& v1, vector<vector<int>>& v2) {
+        // vector<vector<int>>ans;
+        // for(int i=0;i<v2.size();i++){
+        //     v1.push_back(v2[i]);
+        // }
+        // sort(begin(v1),end(v1));
+        // for(auto x:v1) cout<<x[0]<<' '<<x[1]<<"   ";
+        // for(int i=0;i<v1.size()-1;i++){
+        //     if(v1[i][0]==v1[i+1][0]){
+        //         ans.push_back({v1[i][0],v1[i][1]+v1[i+1][1]});
+        //         i++;
+        //     }
+        //     else
+        //     ans.push_back(v1[i]);
+        // }
+        // if(v1[v1.size()-1][0]!=ans[ans.size()-1][0])
+        // ans.push_back(v1[v1.size()-1]);
+        // return ans;
         vector<vector<int>>ans;
-        for(int i=0;i<v2.size();i++){
-            v1.push_back(v2[i]);
+        map<int,int>mp;
+        for(auto x:v1) mp[x[0]]+=x[1];
+        for(auto x:v2)  mp[x[0]]+=x[1];
+        for(auto [key,val]:mp){
+            ans.push_back({key,val});
         }
-        sort(begin(v1),end(v1));
-        for(auto x:v1) cout<<x[0]<<' '<<x[1]<<"   ";
-        for(int i=0;i<v1.size()-1;i++){
-            if(v1[i][0]==v1[i+1][0]){
-                ans.push_back({v1[i][0],v1[i][1]+v1[i+1][1]});
-                i++;
-            }
-            else
-            ans.push_back(v1[i]);
-        }
-        if(v1[v1.size()-1][0]!=ans[ans.size()-1][0])
-        ans.push_back(v1[v1.size()-1]);
+       
         return ans;
     }
 };
