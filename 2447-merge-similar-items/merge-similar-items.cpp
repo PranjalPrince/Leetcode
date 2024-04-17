fc1600c81dff9425c477ce1723ef8009ca3b7583
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-04-17 12:28:10 +0530

    Time: 21 ms (64.54%) | Memory: 20.6 MB (51.70%) - LeetSync

--- 2447-merge-similar-items/merge-similar-items.cpp
+++ 2447-merge-similar-items/merge-similar-items.cpp
@@ -6,7 +6,7 @@ auto speed=[](){
 }();
 class Solution {
 public:
-    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& v1, vector<vector<int>>& v2) {
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
         // vector<vector<int>>ans;
         // for(int i=0;i<v2.size();i++){
         //     v1.push_back(v2[i]);
@@ -24,14 +24,31 @@ public:
         // if(v1[v1.size()-1][0]!=ans[ans.size()-1][0])
         // ans.push_back(v1[v1.size()-1]);
         // return ans;
-        vector<vector<int>>ans;
-        map<int,int>mp;
-        for(auto x:v1) mp[x[0]]+=x[1];
-        for(auto x:v2)  mp[x[0]]+=x[1];
-        for(auto [key,val]:mp){
-            ans.push_back({key,val});
-        }
+        // vector<vector<int>>ans;
+        // map<int,int>mp;
+        // for(auto x:v1) mp[x[0]]+=x[1];
+        // for(auto x:v2)  mp[x[0]]+=x[1];
+        // for(auto [key,val]:mp){
+        //     ans.push_back({key,val});
+        // }
        
+        // return ans;
+        map<int,int> mp;
+        vector<vector<int>> ans;
+        for(int i=0;i<items1.size();i++){
+            mp[items1[i][0]]=items1[i][1];
+        }
+        for(int i=0;i<items2.size();i++){
+            mp[items2[i][0]]+=items2[i][1];
+        }
+        vector<int> temp;
+        for(auto i:mp){
+            temp.push_back(i.first);
+            temp.push_back(i.second);
+            ans.push_back(temp);
+            temp.pop_back();
+            temp.pop_back();
+        }
         return ans;
     }
 };
