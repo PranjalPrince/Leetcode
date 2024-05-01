3e57c4a80987de56de4f7d134f299e80faeaabf5
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-05-01 12:31:34 +0530

    Time: 3 ms (42.21%) | Memory: 7.3 MB (91.39%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,22 @@
+auto speed=[](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+};
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int ind=-1;
+        for(int i=0;i<word.size();i++){
+            if(word[i]==ch){
+                ind=i;
+                break;
+            }
+        }
+        if(ind!=-1){
+            reverse(word.begin(),word.begin()+ind+1);
+        }
+        return word;
+    }
+};
