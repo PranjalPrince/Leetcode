ee579d03e407acf35501f3a65dcd836ac49d41a3
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-05-08 19:00:20 +0530

    Time: 139 ms (5.14%) | Memory: 17.6 MB (5.21%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        vector<string>ans;
+        vector<int>ord=score;
+        sort(rbegin(ord),rend(ord));
+        for(int i=0;i<score.size();i++){
+            int ind;
+            for(int j=0;j<ord.size();j++){
+                if(ord[j]==score[i]){
+                    ind=j+1;
+                }
+            }
+            string s;
+            if(ind==1){
+                s="Gold Medal";
+            }
+            else if(ind==2){
+                s="Silver Medal";
+            }
+            else if(ind==3){
+                s="Bronze Medal";
+            }
+            else {
+                s=to_string(ind);
+            }
+            ans.push_back(s);
+        }
+        return ans;
+    }
+};
