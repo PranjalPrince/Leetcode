12b3a5bb16f6065c6b7b34c88cc700109344f72a
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-06-02 18:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (78.98%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int ans=0;
+        long int score=0;
         for(int i=0;i<s.size()-1;i++){
-            ans+=abs(int(s[i])-int(s[i+1]));
+            score+=abs(int(s[i])-int(s[i+1]));
         }
-        return ans;
+        return score;
     }
 };
