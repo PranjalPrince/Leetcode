e14ffe79399177050e68dc10c9e4c11c06bca9e1
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-07-10 21:16:56 +0530

    Time: 137 ms (41.47%) | Memory: 64.9 MB (92.52%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -1,6 +1,8 @@
+
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
         unordered_map<int,int>mp;
         for(int i=0;i<arr.size();i++){
             mp[arr[i]]++;
@@ -8,9 +10,6 @@ public:
         int fre=0;
         vector<pair<int,int>>v(mp.begin(),mp.end());
         sort(v.begin(),v.end(),[](const auto&a, const auto& b){return a.second<b.second;});
-        // for(auto x:v){
-        //     cout<<x.first<<' '<<x.second<<endl;
-        // }
         for(auto &[key,val]:v){
             if(k>val){
                 k-=val;
