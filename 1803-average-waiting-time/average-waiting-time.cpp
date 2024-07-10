de08f76bf401487d5a2ad8a58fbec03896847ed0
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-07-10 20:07:02 +0530

    Time: 100 ms (100.00%) | Memory: 76.6 MB (58.31%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -7,11 +7,9 @@ auto speed=[](){
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& cust) {
-        // sort(begin(cust),end(cust));
         double t=cust[0][0],ans=0;
         for(int i=0;i<(int)cust.size();i++){
             if(t<cust[i][0]){
-                // ans+=cust[i][0]-t;
                 t=cust[i][0];
             }
             if(t>cust[i][0]){
@@ -19,7 +17,6 @@ public:
             }
             t+=cust[i][1];
             ans+=cust[i][1];
-            cout<<t<<' '<<ans<<endl;
         }
         return double((double)ans/(double)cust.size());
     }
