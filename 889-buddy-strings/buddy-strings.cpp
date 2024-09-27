afa6921c8587aaee48ae27af20f6f7ff33d7c082
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2024-09-27 17:08:46 +0530

    Time: 3 ms (73.05%) | Memory: 8.4 MB (56.69%) - LeetSync

---
+++ 889-buddy-strings/buddy-strings.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.size()!=goal.size())
+        return false;
+        vector<int>v;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=goal[i]){
+                v.push_back(i);
+            }
+        }
+        if(v.size()==2){
+            return s[v[0]]==goal[v[1]] and s[v[1]]==goal[v[0]];
+        }
+        if(v.empty()){
+            for(auto x:s){
+                if(count(s.begin(),s.end(),x)>1)
+                return true;
+            }
+        }
+        return false;
+    }
+};
+//  if (s.size() != goal.size()) return false;
+
+//         std::vector<int> v;
+        
+//         // Find the indices where the characters differ
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] != goal[i]) {
+//                 v.push_back(i);
+//             }
+//         }
+
+//         // If there are exactly two differences, check if they can be swapped
+//         if (v.size() == 2) {
+//             return s[v[0]] == goal[v[1]] && s[v[1]] == goal[v[0]];
+//         }
+        
+//         // If there are no differences, check for duplicates
+//         if (v.empty()) {
+//             std::unordered_set<char> seen;
+//             for (char c : s) {
+//                 if (seen.count(c)) {
+//                     return true; // Found a duplicate character
+//                 }
+//                 seen.insert(c);
+//             }
+//             return false; // No duplicates found
+//         }
+
+//         return false;
+//     }
+// };
