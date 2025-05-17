0f1b51afcc18f20c93faec2514784334454133c8
Pranjal Prince <86613277+PranjalPrince@users.noreply.github.com>
2025-05-17 22:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.80%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,32 +1,24 @@
-auto speed=[](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+
         int a=0,b=0,c=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0)
             a++;
             else if(nums[i]==1)
             b++;
-            else c++;
-        }
-        int j=0;
-        for(int i=0;i<a;i++){
-            nums[j]=0;
-            j++;
-        }
-        for(int i=0;i<b;i++){
-            nums[j]=1;
-            j++;
-        }
-        for(int i=0;i<c;i++){
-            nums[j]=2;
-            j++;
+            else if(nums[i]==2)
+            c++;
+            for(int i=0;i<nums.size();i++){
+                if(i<a)
+                nums[i]=0;
+                else if(i<a+b)
+                nums[i]=1;
+                else if(i<a+b+c)
+                nums[i]=2;
+            }
         }
     }
 };
